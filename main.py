a3289d4ede79995057ba0783dcc642b202b4598e
marsacaroldasilviana <143366384+marsacaroldasilviana@users.noreply.github.com>
2023-10-30 20:36:07 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,61 @@
-# import module matematika math
-import math
-# Input koefisien dari keyboard
-a = int(input("Masukkan a: "))
-b = int(input("Masukkan b: "))
-c = int(input("Masukkan c: "))
-# hitung diskriminan d
-d = (b**2) - (4*a*c)
-# menemukan x1 dan x2
-x1 = (-b+math.sqrt(d))/(2*a)
-x2 = (-b-math.sqrt(d))/(2*a)
-print("Solusinya adalah {0} dan {1}".format(x1, x2))
+#Mencetak Menu
+def menu():
+    print("Pilih Bentuk 2D")
+    print
+    print("1. Persegi Panjang")
+    print("2. Lingkaran")
+    print("3. Segitiga")
+    print("4. Keluar")
+def persegi():
+    print("Menghitung Luas Persegi Panjang")
+    p = int(input("Masukan Panjang :"))
+    l = int(input("Masukkan Lebar : "))
+    luas = p*l
+    print("Luas Persegi Panjang adalah ",luas)
+    print
+    print("Coba lagi [Y/N]?")
+    back = input().upper()
+    if back == "Y":
+        menu()
+    else:
+        exit()
+def lingkaran():
+    print("Menghitung Luas Lingkaran")
+    r = int(input("Masukkan Jari-Jari : "))
+    luas = 3.14*(r**2)
+    print("Luas Lingkaran adalah ",luas)
+    print
+    print("Coba lagi [Y/N]?")
+    back = input().upper()
+    if back == "Y":
+        menu()
+    else:
+        exit()
+def segitiga():
+    print("Menghitung Luas Segitiga")
+    a = int(input("Masukkan Alas : "))
+    t = int(input("Masukkan Tinggi : "))
+    luas = (a*t)/2
+    print("Luas Segitiga adalah ",luas)
+    print
+    print("Coba lagi [Y/N]?")
+    back = input().upper()
+    if back == "Y":
+        menu()
+    else:
+        exit()
+menu()
+while True:
+    menu_choice = input("Pilih bentuk 2D (1/2/3/4): ")
+    if menu_choice == "1":
+        persegi()
+    elif menu_choice == "2":
+        lingkaran()
+    elif menu_choice == "3":
+        segitiga()
+    elif menu_choice == "4":
+        exit()
+    else:
+        print("Pilihan tidak valid. Silakan pilih 1, 2, 3, atau 4.")
+
+
